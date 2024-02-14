5e4038f9d5104dec9a9e4b042d1e87b51a3f074d
jean-rocha-santos <jean.santos@sptech.school>
2024-02-14 11:52:48 -0300

    Criando primeira class

---
+++ Scream Sound/Musica.cs
@@ -0,0 +1,27 @@
+﻿class Musica
+// Em C#, uma classe(class) é uma estrutura de programação que serve como um modelo para criar objetos.
+{
+    public string nome;
+    // public= ficar visivel para abas fora
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+    // bool = Booleana Verdadeiro ou falso 
+
+    public void ExibirFicha()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+
+        if (disponivel)
+        {
+            Console.WriteLine("Disponivel no plano");
+        }
+        else
+        {
+            Console.WriteLine("Adquira no plano plus");
+        }
+
+    }
+}
---
+++ Scream Sound/Program.cs
@@ -0,0 +1,16 @@
+﻿Musica musica1 =  new Musica();
+musica1.nome = "Céu Azul";
+musica1.artista = "Charlie Brown";
+musica1.duracao = 240;
+musica1.disponivel = true;
+
+
+
+Musica musica2= new Musica();
+musica2.nome = "Love Love";
+musica2.artista = "Gilsons";
+musica2.duracao = 280;
+musica2.disponivel = false;
+
+musica1.ExibirFicha();
+musica2.ExibirFicha();
